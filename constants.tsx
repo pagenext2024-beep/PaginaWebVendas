4467798052249c879e15f4643aed6de556aa7cd3
pagenext2024-beep <pagenext2024@gmail.com>
2026-01-08 00:07:52 -0300

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -68,7 +68,7 @@ export const PROJECTS: Project[] = [
     id: '4',
     name: 'Cont100',
     description: 'Fintech moderna com foco em experiência de usuário mobile-first.',
-    image: 'https://i.ibb.co/QF5JRdvx/Captura-de-tela-2026-01-08-000618.png',
+    image: 'https://i.ibb.co/Xfd61PYf/contab.png',
     tag: '',
     link: '/projeto/novabank'
   }
